8f27bb77c8e10eebfb817c6daf5624efc81bb4ac
Sohail Virani <virani.sohail02@gmail.com>
2022-03-23 05:32:31 -0400

    Dealers coding done

--- main.js
+++ main.js
@@ -22,17 +22,21 @@ let tags = {
 let values = {
     playerValue: 0,
     dealerValue: 0,
-    isAce: false,
+    pIsAce: false,
+    dIsAce: false,
 };
 
 let cardNum = () => Math.floor(Math.random() * array.num.length);
 let cardShape = () => Math.floor(Math.random() * array.shape.length);
 let numOfCard = () => array.num[cardNum()];
 let shapeOfCard = () => array.shape[cardShape()];
+tags.hit.disabled = true;
+tags.hold.disabled = true;
 
 
 function dealing() {
-
+    tags.hit.disabled = false;
+    tags.hold.disabled = false;
 
     for (let i = 2; i < 6; i++) {
         let j = i % 2;
@@ -41,20 +45,50 @@ function dealing() {
             let para = document.createElement('p');
             para.id = "para" + i;
             let numOnCard = numOfCard();
+            // let numOnCard = array.num[8];
             para.textContent = numOnCard + " of " + shapeOfCard();
             tags.player.appendChild(para);
             values.playerValue += array.numValue[array.num.indexOf(numOnCard)];
-            pscore.textContent = values.playerValue;
+            pscore.textContent = "Player Total: " + values.playerValue;
+
+            if (values.playerValue === 21) {
+                let result = document.createElement('p');
+                result.textContent = "Lets check out the dealer";
+                result.style.cssText = "background:white;";
+                document.body.appendChild(result);
+                tags.hit.disabled = true;
+                tags.hold.disabled = true;
+
+            } else if (numOnCard === "Ace") {
+                values.pIsAce = true;
+                if (values.playerValue > 21) {
+                    values.playerValue -= 10;
+                    pscore.textContent = "Player Total: " + values.playerValue;
+                }
+            }
         }
         else {
             let para = document.createElement('p');
             para.id = "para" + i;
             let numOnCard = numOfCard();
+            // let numOnCard = array.num[0];
             para.textContent = numOnCard + " of " + shapeOfCard();
             tags.dealer.appendChild(para);
             values.dealerValue += array.numValue[array.num.indexOf(numOnCard)];
-            dscore.textContent = values.dealerValue;
-       
+            dscore.textContent = "Dealer Total: " + values.dealerValue;
+
+            if (numOnCard === "Ace") {
+                values.dIsAce = true;
+                if (values.dealerValue > 21) {
+                    values.dealerValue -= 10;
+                    dscore.textContent = "Dealer Total: " + values.dealerValue;
+                }
+
+            }
+            if (values.playerValue === 21) {
+                onHold();
+            }
+
         }
     }
     tags.deal.disabled = true;
@@ -63,85 +97,135 @@ function dealing() {
 
 
 function hitme() {
-    // Tasks
-    // end game on win and disable everything
-    // add dealer logic 
-    // generalize same logic to function
-    console.log(isAce);
-    let element = document.createElement('p');
-    element.className = "target";
-    let handNum = num[Math.floor(Math.random() * num.length)];
-    // handNum="Ace";
-    let handShape = shape[Math.floor(Math.random() * shape.length)];
-    let hand = handNum + " of " + handShape;
-    document.getElementById("player").appendChild(element);
-    element.textContent = hand;
+    let para = document.createElement('p');
+    para.className = "target";
+    let numOnCard = numOfCard();
+    // let numOnCard = array.num[0];
+    para.textContent = numOnCard + " of " + shapeOfCard();
+    tags.player.appendChild(para);
+    values.playerValue += array.numValue[array.num.indexOf(numOnCard)];
+    pscore.textContent = "Player Total: " + values.playerValue;
 
-    let cardValue = numValue[num.indexOf(handNum)];
-    //set total
-
-
-    playerValue += cardValue;
-
-    //currenthand is ace
-    if (handNum === "Ace") {
-        isAce = true;
-    }
-    //check if total had ace
-    //Check if greater than 21 set cardvalue as 1      
-    if (isAce && playerValue > 21) {
-
-        playerValue -= 10;
-        isAce = false;
-    }
-
-    document.getElementById('pscore').textContent = "Player Total: " + playerValue;
-    //console.log(playerValue);
-    if (playerValue > 21) {
+    if (values.playerValue > 21) {
+        if (values.pIsAce) {
+            values.playerValue -= 10;
+            pscore.textContent = "Player Total: " + values.playerValue;
+            values.pIsAce = false;
+        } else if (numOnCard === "Ace") {
+            values.playerValue -= 10;
+            pscore.textContent = "Player Total: " + values.playerValue;
+            if (values.playerValue === 21) {
+                let result = document.createElement('p');
+                result.textContent = "Lets check out the dealer";
+                result.style.cssText = "background:white;";
+                document.body.appendChild(result);
+                onHold();
+                tags.hit.disabled = true;
+                tags.hold.disabled = true;
+            }
+        }
+        else {
+            let result = document.createElement('p');
+            result.textContent = "You Lost";
+            result.style.cssText = "background:white;";
+            document.body.appendChild(result);
+            tags.hit.disabled = true;
+            tags.hold.disabled = true;
+        }
+    } else if (values.playerValue === 21) {
         let result = document.createElement('p');
-        result.textContent = "You Lose";
+        result.textContent = "Lets check out the dealer";
         result.style.cssText = "background:white;";
         document.body.appendChild(result);
-        document.getElementById('pscore').textContent = "Player Total: " + playerValue;
-        //console.log("You Lose");
-        document.getElementById("hit").disabled = true;
-        document.getElementById("hold").disabled = true;
-    } else if (playerValue == 21) {
-        let result = document.createElement('p');
-        result.textContent = "You win";
-        result.style.cssText = "background:white;";
-        document.body.appendChild(result);
-        document.getElementById("deal").disabled = false;
+        onHold();
+        tags.hit.disabled = true;
+        tags.hold.disabled = true;
     }
 }
 
 
 
 function onHold() {
-    // on hold switch hitme() to dealerValue
-    let hold = document.getElementById('dscore');
-    hold.style.cssText = "display:inline-block;";
-    let para11 = document.getElementById('para11');
-    para11.style.cssText = "display:inline-block;";
-    document.getElementById("hit").disabled = true;
+    //debugger;
+    tags.hold.disabled = true;
+    let para5 = document.getElementById('para5');
+    para5.style.cssText = "display:inline-block;";
+    tags.dscore.style.cssText = "display:inline-block;"
+    tags.hit.disabled = true;
+
+    if (values.dealerValue >= 17 && values.dealerValue < values.playerValue) {
+        let result = document.createElement('p');
+        result.textContent = "You Win";
+        result.style.cssText = "background:white;";
+        document.body.appendChild(result);
+        tags.hit.disabled = true;
+        tags.hold.disabled = true;
+
+    } else if (values.dealerValue >= 17 && values.dealerValue === values.playerValue) {
+        let result = document.createElement('p');
+        result.textContent = "It's a TIE";
+        result.style.cssText = "background:white;";
+        document.body.appendChild(result);
+        tags.hit.disabled = true;
+        tags.hold.disabled = true;
+    } else if (values.dealerValue >= 17 && values.dealerValue > values.playerValue) {
+        let result = document.createElement('p');
+        result.textContent = "You Lose";
+        result.style.cssText = "background:white;";
+        document.body.appendChild(result);
+        tags.hit.disabled = true;
+        tags.hold.disabled = true;
+    } else if (values.dealerValue <= 21 && values.dealerValue > values.playerValue) {
+        let result = document.createElement('p');
+        result.textContent = "You Lose";
+        result.style.cssText = "background:white;";
+        document.body.appendChild(result);
+        tags.hit.disabled = true;
+        tags.hold.disabled = true;
+    } else {
+        // debugger;
+        for (values.dealerValue; values.dealerValue < 17;) {
+            let para = document.createElement('p');
+            para.className = "target";
+            let numOnCard = numOfCard();
+            // let numOnCard = array.num[0];
+            para.textContent = numOnCard + " of " + shapeOfCard();
+            tags.dealer.appendChild(para);
+            values.dealerValue += array.numValue[array.num.indexOf(numOnCard)];
+            dscore.textContent = "Dealer Total: " + values.dealerValue;
+
+            if (values.dealerValue > 21) {
+                if (values.dIsAce) {
+                    values.dealerValue -= 10;
+                    dscore.textContent = "Dealer Total: " + values.dealerValue;
+                    values.dIsAce = false;
+                } else if (numOnCard === "Ace") {
+                    values.dealerValue -= 10;
+                    dscore.textContent = "Dealer Total: " + values.dealerValue;
+                    if (values.dealerValue === 21) {
+                        onHold();
+                    }
+                } else if (values.dealerValue >= 17 && values.dealerValue < 21) {
+                    onHold(a);
+                }
+                else {
+                    let result = document.createElement('p');
+                    result.textContent = "You Win";
+                    result.style.cssText = "background:white;";
+                    document.body.appendChild(result);
+                    tags.hit.disabled = true;
+                    tags.hold.disabled = true;
+                }
+            }else if (values.dealerValue >= 17 && values.dealerValue < 21) {
+                onHold();
+            } else if (values.dealerValue === 21) {
+                onHold();
+            }
+        }
+    }
 
 }
 
 deal.addEventListener('click', dealing);
 hit.addEventListener('click', hitme);
 hold.addEventListener('click', onHold);
-
-
-// if (values.playerValue == 21 && values.dealerValue == 21) {
-//     let result = document.createElement('p');
-//     result.textContent = "It's a Tie";
-//     result.style.cssText = "background:white;";
-//     document.body.appendChild(result);
-
-// } else if (values.playerValue == 21) {
-//     let result = document.createElement('p');
-//     result.textContent = "You Win";
-//     result.style.cssText = "background:white;";
-//     document.body.appendChild(result);
-
-// } 
